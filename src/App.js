db04a5f88f4964e0a636b8aa546340938cbe0890
redzzzi <myqueen1346@naver.com>
2024-08-11 00:17:32 +0900

    setting : CRA와 코드 포매터

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -11,12 +13,13 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
