64b140869de3a05de3fb6355bc27182919937724
redzzzi <myqueen1346@naver.com>
2024-08-16 15:40:01 +0900

    feature : header 구현하기 - Material UI의 아이콘 사용 - MainLayout.jsx 파일 생성 후 Header.jsx 렌더링

--- src/components/Layout/Sidebar/Sidebar.jsx
+++ src/components/Layout/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PICKLE_COLOR, PICKLE_WIDTH } from 'constants/pickleTheme';
 
 const Sidebar = () => {
   return <div></div>;
--- src/router/useMainRouter.jsx
+++ src/router/useMainRouter.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
-import Hello from '../components/Hello';
-import Group from '../components/Layout/Group/Group';
-import Sidebar from '../components/Layout/Sidebar/Sidebar';
+import MainLayout from 'components/Layout/MainLayout';
+import Hello from 'components/Hello';
+import Group from 'components/Layout/Group/Group';
+import Header from 'components/Layout/Header/Header';
+import Sidebar from 'components/Layout/Sidebar/Sidebar';
 
 const useMainRouter = () => {
   return useRoutes([
-    { path: '/', element: <Hello /> },
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        {
+          element: <Hello />,
+        },
+      ],
+    },
     { path: '/Group', element: <Group /> },
+    { path: '/header', element: <Header /> },
     { path: '/sidebar', element: <Sidebar /> },
   ]);
 };
