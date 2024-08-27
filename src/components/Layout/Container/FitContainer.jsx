f1efb1f12b9e728a6fb7c5c7d746480253fe5266
redzzzi <myqueen1346@naver.com>
2024-08-27 16:54:21 +0900

    style : 코드 정리

--- src/components/Layout/Container/FitContainer.jsx
+++ src/components/Layout/Container/FitContainer.jsx
@@ -5,7 +5,7 @@ const FitContainer = () => {
   return (
     <div className="flex justify-center w-full min-h-screen pt-10 sm:pt-16">
       <div className="w-full px-2 mb-20 mt-header max-w-container 2xl:px-0">
-        <Outlet />;
+        <Outlet />
       </div>
     </div>
   );
--- src/components/Layout/Container/FullContainer.jsx
+++ src/components/Layout/Container/FullContainer.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const FullContainer = () => {
   return (
-    <div className="flex justify-center w-full min-h-screen pt-14 sm:pt-header">
+    <div className="flex justify-center w-full min-h-screen sm:pt-6">
       <Outlet />
     </div>
   );
