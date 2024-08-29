e844f907729140556cf2fc1aacdcf03ab3035812
myeowon <myw7931@gmail.com>
2024-08-29 17:47:34 +0900

    feature:프로젝트 페이지 이미지 슬라이드

---
+++ src/components/Project/ProjectDomainAddress.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Box, Typography, IconButton, Stack } from '@mui/material';
+import LinkIcon from '@mui/icons-material/Link';
+import { PICKLE_COLOR } from 'constants/pickleTheme';
+
+const ProjectDomainAddress = (props) => {
+  const url = props.projectDomainAddress;
+  const handleIconClick = () => {
+    window.location.href = url;
+  };
+  const handleTextClick = () => {
+    navigator.clipboard.writeText(url).then(() => {
+      alert('URL copied to clipboard!');
+    });
+  };
+  return (
+    <Stack
+      direction="row"
+      spacing={2}
+      sx={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <IconButton onClick={handleIconClick} sx={{ width: 27, height: 27 }}>
+        <LinkIcon sx={{ color: PICKLE_COLOR.pointOrange, fontSize: 35 }} />
+      </IconButton>
+      <Box
+        sx={{
+          alignItems: 'center',
+          textAlign: 'center',
+
+          width: 415,
+          height: 42,
+          padding: '5px',
+
+          border: '1px solid #BFBFBF',
+          borderRadius: '999px',
+        }}
+      >
+        <Typography
+          sx={{
+            marginLeft: '8px',
+            cursor: 'pointer',
+            flexGrow: 1,
+
+            fontWeight: 500,
+            fontSize: 15,
+
+            alignItems: ' center',
+            textAlign: 'center',
+          }}
+          onClick={handleTextClick}
+        >
+          {url}
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+export default ProjectDomainAddress;
---
+++ src/components/Project/ProjectPics.jsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import './ImageSlider.css';
+import { IconButton } from '@mui/material';
+import { PICKLE_COLOR } from 'constants/pickleTheme';
+
+const ProjectPics = (props) => {
+  const images = props.projectPictures;
+  const settings = {
+    centerMode: true,
+    centerPadding: '200px',
+    slidesToShow: 1,
+    focusOnSelect: true,
+    dots: true,
+    infinite: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="container">
+      <Slider {...settings} className="center">
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className="cl"
+            sx={{
+              borderRadius: '10px',
+              border: '1px solid #BFBFBF',
+              filter: 'drop-shadow(0px 4px 9px rgba(0, 0, 0, 0.25))',
+              width: 400,
+              height: 300,
+            }}
+          >
+            <img src={image.src} alt={image.alt} width="400px" height="300px" />
+          </div>
+        ))}
+      </Slider>
+      <div className="pagination"></div>
+    </div>
+  );
+};
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <IconButton
+      onClick={onClick}
+      sx={{ position: 'absolute', top: '50%', right: '-30px', zIndex: 1, color: PICKLE_COLOR.pointOrange }}
+    >
+      <ArrowForwardIosIcon />
+    </IconButton>
+  );
+};
+
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <IconButton
+      onClick={onClick}
+      sx={{ position: 'absolute', top: '50%', left: '-30px', zIndex: 1, color: PICKLE_COLOR.pointOrange }}
+    >
+      <ArrowBackIosIcon />
+    </IconButton>
+  );
+};
+export default ProjectPics;
+// import React from 'react';
+// import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+//
+
+// import './ImageSlider.css';
+// import { Box } from '@mui/material';
+// //https://codepen.io/mackDEV2/pen/jOrpPRY
+// //https://codepen.io/badcomet/pen/QWWpVgX
+// const ProjectPics = (props) => {
+//   const images = props.projectPictures;
+
+//   const settings = {
+//     centerMode: true,
+//     centerPadding: '200px',
+//     slidesToShow: 1,
+//     infinite: true,
+//     dots: true, // 슬라이더 아래에 네비게이션 점 표시
+//     //speed: 500,
+//     // slidesToScroll: 1,
+//     // 한 번에 스크롤할 슬라이드 수
+
+//     focusOnSelect: true, // 클릭한 슬라이드로 포커스를 이동시킵니다.
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1,
+//         },
+//       },
+//     ],
+//   };
+
+//   return (
+//     <div className="container">
+//       <Slider {...settings} className="center slider">
+//         <div className="slide">
+//           {images.map((image) => (
+//             <Card
+//               key={image.id}
+//               className="slider-item frame"
+//               sx={{
+//                 borderRadius: '10px',
+//                 border: '1px solid #BFBFBF',
+//                 filter: 'drop-shadow(0px 4px 9px rgba(0, 0, 0, 0.25))',
+//               }}
+//             >
+//               <CardMedia
+//                 component="img"
+//                 height="194"
+//                 image={image.src}
+//                 alt={image.alt}
+//                 className="slider-item frame"
+//                 sx={{
+//                   borderRadius: '10px',
+//                 }}
+//               />
+//             </Card>
+//           ))}
+//         </div>
+//       </Slider>
+//     </div>
+//   );
+// };
+
+// export default ProjectPics;
